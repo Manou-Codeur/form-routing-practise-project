de924633fdd9a251e41a9d517d2d586da3011525
khaled-codeur <manou-codeur@outlook.com>
2020-06-26 22:17:38 +0100

    first app done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
-
-import Input from "./common/Input";
+import Form from "./common/from";
 
 import "./App.css";
 
-class App extends Component {
+class App extends Form {
   state = {
-    account: {
+    data: {
       userName: "",
       password: "",
     },
@@ -16,73 +15,24 @@ class App extends Component {
 
   schema = {
     userName: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password"),
+    password: Joi.string().required().min(10).label("Password"),
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const result = Joi.validate(this.state.account, this.schema, options);
-
-    if (!result.error) return null;
-
-    const errors = {};
-    for (let els of result.error.details) {
-      errors[els.path[0]] = els.message;
-    }
-    return errors;
-  };
-
-  preventDefault = e => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     console.log("call the server");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  updateUserState = ({ target: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMsg = this.validateProperty(input);
-    if (errorMsg) errors[input.name] = errorMsg;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
-
     return (
       <form onSubmit={this.preventDefault}>
-        <Input
-          label="User Name"
-          name="userName"
-          value={account.userName}
-          onChange={this.updateUserState}
-          type="text"
-          error={errors.userName}
-        />
-        <Input
-          label="Password"
-          name="password"
-          value={account.password}
-          onChange={this.updateUserState}
-          type="password"
-          error={errors.password}
-        />
+        {this.renderInput("Username", "userName")}
+        {this.renderInput("Password", "password", "password")}
 
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={this.validate()}
+        >
           Submit
         </button>
       </form>
--- src/common/Input.jsx
+++ src/common/Input.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-const Input = ({ label, name, value, onChange, type, error }) => {
+const Input = ({ label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={label}>{label}</label>
       <input
-        type={type}
         className="form-control"
         id={label}
-        name={name}
+        {...rest}
         aria-describedby="emailHelp"
-        value={value}
-        onChange={onChange}
       />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
